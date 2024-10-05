3d72bf58eb6ea102d92065ba3923408ba5784873
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-10-06 02:35:40 +0300

    - Make Configuration to Depoly on Vercel.com

--- CharactersProject/settings.py
+++ CharactersProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3_np)^8j1om78fk22(15y$#5%oi5(#_3ji!$tl6s2!ch2glm3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
